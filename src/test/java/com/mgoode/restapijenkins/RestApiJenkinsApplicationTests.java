39ae6cd7083ae0042ac5e5f09ab445b7d80c6f1a
michaelgoode <michaelgoode66@googlemail.com>
2020-02-12 13:07:33 +0000

    xxxxx

--- src/test/java/com/mgoode/restapijenkins/RestApiJenkinsApplicationTests.java
+++ src/test/java/com/mgoode/restapijenkins/RestApiJenkinsApplicationTests.java
@@ -1,5 +1,7 @@
 package com.mgoode.restapijenkins;
 
+import com.mgoode.restapijenkins.service.Calculator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,15 @@ class RestApiJenkinsApplicationTests {
 
     @Test
     void contextLoads() {
+
+    }
+
+    @Test
+    void testCalculator() {
+        Calculator calculator = new Calculator();
+        Assertions.assertEquals(40, calculator.add(20, 20));
+        Assertions.assertEquals(60, calculator.multiply(15, 4));
+        Assertions.assertEquals(20, calculator.subtract(60, 40));
     }
 
 }
