0e081984cd5800763699182cb06f18afeef58324
michaelgoode <michaelgoode66@googlemail.com>
2020-02-12 12:00:25 +0000

    xxxxx

---
+++ src/main/java/com/mgoode/restapijenkins/RestApiJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.mgoode.restapijenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mgoode/restapijenkins/RestApiJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgoode.restapijenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
